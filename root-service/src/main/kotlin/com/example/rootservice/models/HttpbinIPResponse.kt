107c3b1a0a8cd63659036ec00014e8e8f9e14c92
HarshVaragiya <harshvaragiya8@gmail.com>
2020-05-12 16:41:26 +0530

    SOCKS Error needs fixing

---
+++ root-service/src/main/kotlin/com/example/rootservice/models/HttpbinIPResponse.kt
@@ -0,0 +1,5 @@
+package com.example.rootservice.models
+
+class HttpbinIPResponse {
+    var origin: String = ""
+}
--- root-service/src/main/kotlin/com/example/rootservice/resource/BooksResource.kt
+++ root-service/src/main/kotlin/com/example/rootservice/resource/BooksResource.kt
@@ -1,15 +1,17 @@
 package com.example.rootservice.resource
 
 import com.example.rootservice.models.BookInfoResponse
+import com.example.rootservice.models.HttpbinIPResponse
 import com.example.rootservice.models.UserBooksResponse
 import com.example.rootservice.models.UserRequestResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
 import javax.servlet.http.HttpServletRequest
+
 
 @RestController
 @RequestMapping("/book/")
@@ -20,8 +22,10 @@ class BooksResource() {
     var logger: Logger = LoggerFactory.getLogger((BooksResource::class.java))
 
     @RequestMapping("/getInfo/{username}")
-    fun getBookInfo(@PathVariable("username") username: String, request: HttpServletRequest):UserRequestResponse{
+    fun getBookInfo(@PathVariable("username") username: String, request: HttpServletRequest, @RequestHeader headers: Map<String, String>):UserRequestResponse{
         logger.warn("[+] Incoming Request from ${request.remoteAddr} for user $username")
+        headers.forEach { (key: String?, value: String?) -> logger.warn(String.format("Request Header '%s' => %s", key, value)) }
+
         val responseObject : UserRequestResponse = UserRequestResponse()
         responseObject.username = username
         val userBooksObject : UserBooksResponse = UserBooksResponse()
@@ -34,12 +38,18 @@ class BooksResource() {
         }
 
         for (bookId in userBooksObject.booksRead){
-            restTemplate.getForObject("http://localhost:8001/library/$bookId",BookInfoResponse::class.java)?.let { responseObject.booksRead.add(it) };
+            restTemplate.getForObject("http://yit7m3eeuotbmpzq.onion:8001/library/$bookId",BookInfoResponse::class.java)?.let { responseObject.booksRead.add(it) };
         }
         for (bookId in userBooksObject.booksToRead){
-            restTemplate.getForObject("http://localhost:8001/library/$bookId",BookInfoResponse::class.java)?.let { responseObject.booksToRead.add(it) };
+            restTemplate.getForObject("http://yit7m3eeuotbmpzq.onion:8001/library/$bookId",BookInfoResponse::class.java)?.let { responseObject.booksToRead.add(it) };
         }
         return responseObject
+    }
+
+    @RequestMapping("/getProxyIp")
+    fun getProxyIp():HttpbinIPResponse{
+        val response : HttpbinIPResponse = restTemplate.getForObject("http://httpbin.org/ip",HttpbinIPResponse::class.java)!!
+        return response
     }
 
 
